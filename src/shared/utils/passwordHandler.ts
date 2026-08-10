40bf5729335924e58db648b422b6421e36bdd94a
gustavo <g251634@gmail.com>
2026-08-10 19:38:05 -0300

    Feat(auth): add util/passwordHandler

---
+++ src/shared/utils/passwordHandler.ts
@@ -0,0 +1,25 @@
+import { hash, genSalt, compare } from "bcrypt";
+import { HttpError } from "../../errors/httpError.ts";
+
+export async function hashPassword(password: string) {
+
+    try {
+        const salt = await genSalt(10);
+
+        const hashedPassword = await hash(password, salt);
+
+        return hashedPassword;
+    } catch (error) {
+        throw new HttpError("Erro ao gerar hash da senha", 400);
+    }
+}
+
+export async function comparePassword(password: string, hashedPassword: string) {
+    try {
+        const match = await compare(password, hashedPassword);
+
+        return match;
+    } catch (error) {
+        throw new HttpError("Erro ao compara as senhas:", 400);
+    }
+}
