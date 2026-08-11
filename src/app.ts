c2a0502da6403853bf981e6b3c2aa5f2525b9e26
Lucas Gomes <lucasg3984@gmail.com>
2026-08-11 20:19:27 -0300

    feat(cors): allow cross-origin credentialed requests

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,22 @@
 import express from "express";
 import router from "./router.ts";
+import cors from "cors";
 import { errorHandler } from "./shared/middleware/errorHandler.ts";
+import Config from "./configs.ts";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: Config.CORS_ORIGINS,
+    credentials: true,
+  }),
+);
 
 app.disable("x-powered-by");
 app.use(express.json());
 
 app.use(router);
-
-
-
-
-
-
 
 //rotas sempre acima disso:
 app.use(errorHandler);
