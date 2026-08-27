cf3b70dfb7de233863a81d10557d5ef0334251e5
Felipe Gutierrez <lipegutierrez27@gmail.com>
2026-08-27 18:35:51 -0300

    feat(requests): add requests router

---
+++ src/modules/requests/requests.router.ts
@@ -0,0 +1,6 @@
+import express from "express";
+const requestsRoutes = express.Router();
+
+requestsRoutes.post('/', request);
+
+export default requestsRoutes
--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import authRoutes from "./modules/auth/auth.router.ts";
+import requestsRoutes from "./modules/requests/requests.router.ts";
 
 const router = Router();
 
 router.use("/auth", authRoutes);
+router.use('/requests', requestsRoutes);
 
 export default router;
