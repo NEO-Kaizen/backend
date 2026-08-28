86c304d842ff6b9d29386a4d499a021c2f978f9d
VictorJob <victorjobnogueira12@gmail.com>
2026-08-28 06:36:08 -0300

    feat(request): add public request tracking routes

--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import authRoutes from "./modules/auth/auth.router.ts";
+import requestRoutes from "./modules/requests/requests.router.ts"
 
 const router = Router();
 
 router.use("/auth", authRoutes);
+
+router.use("/request", requestRoutes)
 
 export default router;
