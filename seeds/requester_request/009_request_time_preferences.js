e201d1df894ed78c8a366bf48b419b8f857f4fc5
Igor Gabriel Oliveira <igorgabrieloliveira88@gmail.com>
2026-09-14 03:39:02 -0300

    chore(seeds): seed pending items, attachments and time preferences

--- seeds/requester_request/009_request_time_preferences.js
+++ seeds/requester_request/009_request_time_preferences.js
@@ -1,7 +1,19 @@
 export async function seed(knex) {
   await knex("request_time_preferences").insert([
-    { request_id: 1, scheduled_for: "2026-09-02 08:00:00" },
-    { request_id: 1, scheduled_for: "2026-09-02 10:00:00" },
-    { request_id: 1, scheduled_for: "2026-09-03 14:00:00" },
+    { request_id: 7, scheduled_for: "2026-09-08 10:00:00" },
+    { request_id: 7, scheduled_for: "2026-09-08 14:00:00" },
+    { request_id: 7, scheduled_for: "2026-09-10 09:00:00" },
+    { request_id: 8, scheduled_for: "2026-09-20 14:00:00" },
+    { request_id: 8, scheduled_for: "2026-09-21 09:00:00" },
+    { request_id: 9, scheduled_for: "2026-09-15 11:00:00" },
+    { request_id: 9, scheduled_for: "2026-09-16 09:30:00" },
+    { request_id: 10, scheduled_for: "2026-09-22 10:30:00" },
+    { request_id: 10, scheduled_for: "2026-09-23 15:00:00" },
+    { request_id: 11, scheduled_for: "2026-09-24 09:00:00" },
+    { request_id: 12, scheduled_for: "2026-09-25 14:30:00" },
+    { request_id: 14, scheduled_for: "2026-10-01 10:00:00" },
+    { request_id: 17, scheduled_for: "2026-10-06 09:00:00" },
+    { request_id: 18, scheduled_for: "2026-10-05 15:00:00" },
+    { request_id: 19, scheduled_for: "2026-10-09 11:00:00" },
   ]);
 }
