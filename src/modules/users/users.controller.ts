a32b8e2848ca5c7fdac43ae1f5f8e413f975ab5f
Igor Gabriel <143961257+igorgabrieloliveira@users.noreply.github.com>
2026-09-14 01:45:01 -0300

    fix:fixing imports (#74)

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -3,12 +3,6 @@ import { AppError } from "../../shared/errors/AppError.ts";
 import { formatZodIssues } from "../../shared/validation/zodErrors.ts";
 import { changeUserStatusSchema, createUserSchema, listUsersQuerySchema } from "./users.schema.ts";
 
-import {
-  changeUserStatusSchema,
-  createUserSchema,
-  listUsersQuerySchema,
-} from "./users.schema.ts";
-
 import * as service from "./users.service.ts";
 
 function actorUserId(req: Request): number {
