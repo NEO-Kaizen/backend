99de54cc05185bf9f4fbd297d06a1f1c56e6ee71
VictorJob <victorjobnogueira12@gmail.com>
2026-09-19 23:34:19 -0300

    feat(triage): adds triage routes to router.ts

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import authRoutes from "./modules/auth/auth.router.ts";
 import requestsRoutes from "./modules/requests/requests.router.ts";
+import triageRoutes from "./modules/requests/triage/triage.router.ts";
 import usersRoutes from "./modules/users/users.router.ts";
 import queueRoutes from "./modules/queue/queue.router.ts";
 import prioritizationRoutes from "./modules/prioritization/prioritization.router.ts";
@@ -11,6 +12,7 @@ const router = Router();
 router.use("/prioritization", prioritizationRoutes);
 
 router.use("/auth", authRoutes);
+router.use("/requests", triageRoutes);
 router.use("/requests", requestsRoutes);
 router.use("/users", usersRoutes);
 
